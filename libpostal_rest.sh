dfd189ba147f9d52ce40ee09a90e6cb121e3537f
eazyfin <84118641+eazyfin@users.noreply.github.com>
2022-03-14 18:14:15 +0530

    testing

--- libpostal_rest.sh
+++ libpostal_rest.sh
@@ -4,10 +4,12 @@
 curl https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz > go1.8.linux-amd64.tar.gz
 tar xzf go1.8.linux-amd64.tar.gz
 
+apt-get update
+apt-get install golang
 # Set Paths.
-export GOROOT=/libpostal/go
-export GOPATH=/libpostal/workspace
-export PATH=$PATH:/libpostal/go/bin
+# export GOROOT=/libpostal/go
+# export GOPATH=/libpostal/workspace
+# export PATH=$PATH:/libpostal/go/bin
 
 # Get go script.
 go get github.com/eazyfin/libpostal-rest
